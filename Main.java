b43a645e055c6ffe5d13455ed0ed45b1fbb2aa66
AgroGn0m <154757009+AgroGn0m@users.noreply.github.com>
2023-12-24 21:33:50 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,118 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите операцию");
+        String input = scanner.nextLine();
+        System.out.println(calcilate(input));
+
+    }
+
+    public static  String calcilate(String input) throws Exception {
+        String simvol;
+        int first_number;
+        String result;
+        int second_number;
+        boolean search_number;
+        String[] h = input.split("[+\\-+/]");
+
+        // ---- Вычисляем кол-во операндов
+
+        if (h.length != 2) throw new Exception("Error: Вводите только одну арифметическую операцию и два числа.");
+
+        // ---- Находим символ операции
+        simvol = search_Operation(input);
+        if (simvol == null) throw  new Exception("Введите арифметическую опреацию!");
+        // ---- Определяем принадлежность числел
+
+        if (Rim.search_number(h[0]) && (Rim.search_number(h[1]))) {
+            first_number = Rim.convert(h[0]);
+            second_number = Rim.convert(h[1]);
+            search_number = true;
+
+        } else if (!Rim.search_number(h[0]) && !Rim.search_number(h[1])) {
+            first_number = Integer.parseInt(h[0]);
+            second_number = Integer.parseInt(h[1]);
+            search_number = false;
+        } else {
+            throw new Exception("Числа должны быть одного типа!");
+        }
+
+        // --- Проверяем числа
+
+        if (first_number > 10 || first_number < 1) {
+            throw new Exception("Error: Число " + first_number + " больше 10-ти или меньше 1-го.");
+           }
+        else {
+            if (second_number > 10 || second_number < 1)
+                throw new Exception("Error: Число " + second_number + " больше 10-ти или меньше 1-го.");
+        }
+        result = String.valueOf(calc(first_number, second_number, simvol));
+        if (search_number) {
+           int result1 = Integer.parseInt(result);
+            if (result1 <=0) {
+                throw new Exception("Римское число должно быть больше нуля");
+            }
+            result = Rim.convertToRim(result1);
+        }
+
+        return result;
+    }
+    static String search_Operation(String input) {
+        if (input.contains("+")) {
+            return "+";
+        } else if (input.contains("-")) {
+            return "-";
+        } else if (input.contains("/")) {
+            return "/";
+        } else if (input.contains("*")) {
+            return "*";
+        } else return null;
+
+    }
+    static int calc(int a, int b, String simvol) {
+
+        if (simvol.equals("+")) return a + b;
+        else if (simvol.equals("-")) return a - b;
+        else if (simvol.equals("*")) return a * b;
+        else return a / b;
+    }
+}
+
+class Rim {
+
+    static String[] array = new String[]{"0", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI",
+            "XII", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII", "XIX", "XX", "XXI", "XXII", "XXIII", "XXIV",
+            "XXV", "XXVI", "XXVII", "XXVIII", "XXIX", "XXX", "XXXI", "XXXII", "XXXIII", "XXXIV", "XXXV", "XXXVI",
+            "XXXVII", "XXXVIII", "XXXIX", "XL", "XLI", "XLII", "XLIII", "XLIV", "XLV", "XLVI", "XLVII", "XLVIII",
+            "XLIX", "L", "LI", "LII", "LIII", "LIV", "LV", "LVI", "LVII", "LVIII", "LIX", "LX", "LXI", "LXII",
+            "LXIII", "LXIV", "LXV", "LXVI", "LXVII", "LXVIII", "LXIX", "LXX", "LXXI", "LXXII", "LXXIII", "LXXIV",
+            "LXXV", "LXXVI", "LXXVII", "LXXVIII", "LXXIX", "LXXX", "LXXXI", "LXXXII", "LXXXIII", "LXXXIV", "LXXXV",
+            "LXXXVI", "LXXXVII", "LXXXVIII", "LXXXIX", "XC", "XCI", "XCII", "XCIII", "XCIV", "XCV", "XCVI", "XCVII",
+            "XCVIII", "XCIX", "C"};
+
+
+    public static boolean search_number(String numbers) {
+        for (int i = 0; i < array.length; i++) {
+            if (numbers.equals(array[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static int convert(String num) {
+        for (int i = 0; i < array.length; i++){
+            return i;
+        }
+        return -1;
+    }
+
+    public static String convertToRim(int result) {
+        return array[result];
+    }
+}
+
+
